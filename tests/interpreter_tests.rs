075ea370443bbd36e65baaeb89ac8fecd05c6482
Adrian Sieber <mail@adriansieber.com>
2024-09-08 19:42:31 +0000

    Add tests for division

--- tests/interpreter_tests.rs
+++ tests/interpreter_tests.rs
@@ -1,46 +1,112 @@
 use woxi::interpret;
 
-#[test]
-fn test_addition() {
-  assert_eq!(interpret("1 + 2").unwrap(), 3.0);
-}
+mod interpreter_tests {
+  use super::*;
 
-#[test]
-fn test_subtraction() {
-  assert_eq!(interpret("3 - 1").unwrap(), 2.0);
-}
+  mod arithmetic {
+    use super::*;
 
-#[test]
-fn test_multiple_operations() {
-  assert_eq!(interpret("1 + 2 - 3 + 4").unwrap(), 4.0);
-}
+    mod integer {
+      use super::*;
 
-#[test]
-fn test_decimal_numbers() {
-  assert_eq!(interpret("1.5 + 2.7").unwrap(), 4.2);
-}
+      #[test]
+      fn addition() {
+        assert_eq!(interpret("1 + 2").unwrap(), 3.0);
+      }
 
-#[test]
-fn test_negative_numbers() {
-  assert_eq!(interpret("-1 + 3").unwrap(), 2.0);
-}
+      #[test]
+      fn subtraction() {
+        assert_eq!(interpret("3 - 1").unwrap(), 2.0);
+      }
 
-#[test]
-fn test_error_invalid_input() {
-  assert!(interpret("1 + ").is_err());
-}
+      #[test]
+      fn multiple_operations() {
+        assert_eq!(interpret("1 + 2 - 3 + 4").unwrap(), 4.0);
+      }
 
-#[test]
-fn test_error_unknown_operator() {
-  assert!(interpret("1 * 2").is_err());
-}
+      #[test]
+      fn negative_numbers() {
+        assert_eq!(interpret("-1 + 3").unwrap(), 2.0);
+      }
 
-#[test]
-fn test_calculation() {
-  assert_eq!(interpret("1 + 2").unwrap(), 3.0);
-}
+      #[test]
+      fn multiplication() {
+        assert_eq!(interpret("3 * 4").unwrap(), 12.0);
+      }
 
-#[test]
-fn test_complex_calculation() {
-  assert_eq!(interpret("1 + 2 - 3 + 4").unwrap(), 4.0);
+      #[test]
+      fn complex_multiplication() {
+        assert_eq!(interpret("2 * 3 + 4 * 5").unwrap(), 26.0);
+      }
+
+      #[test]
+      fn division() {
+        assert_eq!(interpret("10 / 2").unwrap(), 5.0);
+      }
+
+      #[test]
+      fn division_repeating_decimal() {
+        // TODO: Should be kept as the fraction 10/3
+        assert_eq!(interpret("10 / 3").unwrap(), 3.3333333333333334);
+      }
+
+      #[test]
+      fn complex_division() {
+        assert_eq!(interpret("10 / 2 + 3 / 3").unwrap(), 6.0);
+      }
+    }
+
+    mod float {
+      use super::*;
+
+      #[test]
+      fn addition() {
+        assert_eq!(interpret("1.5 + 2.7").unwrap(), 4.2);
+      }
+
+      #[test]
+      fn subtraction() {
+        assert_eq!(interpret("3.5 - 1.2").unwrap(), 2.3);
+      }
+
+      #[test]
+      fn multiple_operations() {
+        assert_eq!(interpret("1.1 + 2.2 - 3.3 + 4.4").unwrap(), 4.4);
+      }
+
+      #[test]
+      fn multiplication() {
+        assert_eq!(interpret("1.5 * 2.5").unwrap(), 3.75);
+      }
+
+      #[test]
+      fn complex_multiplication() {
+        assert_eq!(interpret("1.5 * 2.0 + 3.0 * 1.5").unwrap(), 7.5);
+      }
+
+      #[test]
+      fn division() {
+        // TODO: Should be 3.2
+        assert_eq!(interpret("9.6 / 3").unwrap(), 3.1999999999999997);
+      }
+
+      #[test]
+      fn complex_division() {
+        // TODO: Should be 4.2
+        assert_eq!(interpret("9.6 / 3 + 3.0 / 3").unwrap(), 4.199999999999999);
+      }
+    }
+  }
+
+  mod errors {
+    use super::*;
+
+    #[test]
+    fn invalid_input() {
+      match interpret("1 + ") {
+        Err(woxi::InterpreterError::ParseError(_)) => (),
+        _ => panic!("Expected a ParseError"),
+      }
+    }
+  }
 }
