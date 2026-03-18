e7ffa7316d3a6dcf65669e683ec2b558ae8e00ed
Adrian Sieber <mail@adriansieber.com>
2026-03-18 15:12:30 +0000

    Implement DiscreteDelta and 30 unevaluated symbolic wrappers
    - DiscreteDelta[n1,n2,...]: returns 1 if all args are 0, else 0
    - 30 unevaluated wrappers: Magnify, ScriptBaselineShifts, LineSpacing,
      FunctionRange, Vectors, SectorOrigin, MaxTrainingRounds, PolarAxes,
      PolynomialGCD, SystemDialogInput, ARProcess, DiscreteWaveletTransform,
      RelationGraph, ImagePartition, PetersenGraph, RSolveValue, FeatureExtraction,
      GraphDistance, CellStyle, DirectoryQ, ImageIdentify, Asymptotic,
      CoordinateTransform, WindowMargins, AffineTransform, RadioButton,
      LegendMarkers, PowersRepresentations, ShowStringCharacters

--- src/evaluator/dispatch/complex_and_special.rs
+++ src/evaluator/dispatch/complex_and_special.rs
@@ -24,6 +24,9 @@ pub fn dispatch_complex_and_special(
     "DiracDelta" if !args.is_empty() => {
       return Some(crate::functions::math_ast::dirac_delta_ast(args));
     }
+    "DiscreteDelta" => {
+      return Some(crate::functions::math_ast::discrete_delta_ast(args));
+    }
     "UnitBox" if args.len() == 1 => {
       return Some(crate::functions::math_ast::unit_box_ast(args));
     }
