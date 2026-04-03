3cb6bd256018b5eeeff04ebf81b330c25fa56804
Adrian Sieber <mail@adriansieber.com>
2026-04-03 14:52:18 +0000

    Implement PowersRepresentations (sums of powers)
    PowersRepresentations[n, k, p] gives all representations of n as a sum
    of k non-negative integers each raised to the power p. Uses recursive
    search with pruning for efficiency.

--- src/evaluator/dispatch/arg_count.rs
+++ src/evaluator/dispatch/arg_count.rs
@@ -701,6 +701,7 @@ pub fn get_arg_count_range(name: &str) -> Option<(usize, usize)> {
     "PermutationSupport" => Some((1, 1)),
     "PerfectNumber" => Some((1, 1)),
     "RamanujanTau" => Some((1, 1)),
+    "PowersRepresentations" => Some((3, 3)),
     "Pick" => Some((2, 3)),
     "Piecewise" => Some((1, 2)),
     "PlanarAngle" => Some((1, 1)),
