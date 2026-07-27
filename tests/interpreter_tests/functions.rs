25998c0e5141b08a0d7c7970978679458e8c77bb
kevincha99 <kevincha99@hotmail.com>
2026-07-27 01:19:49 -0700

    Fix file_exists_q::existing_path test on Windows
    Another case of Windows not have a /tmp directory.

--- tests/interpreter_tests/functions.rs
+++ tests/interpreter_tests/functions.rs
@@ -2871,7 +2871,11 @@ mod file_exists_q {
 
   #[test]
   fn existing_path() {
-    assert_eq!(interpret("FileExistsQ[\"/tmp\"]").unwrap(), "True");
+    let path = temp_file("");
+    assert_eq!(
+      interpret(&format!("FileExistsQ[\"{path}\"]")).unwrap(),
+      "True"
+    );
   }
 
   #[test]
